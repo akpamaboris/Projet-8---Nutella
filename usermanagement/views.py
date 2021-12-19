8f754729c26de7ec5f7cedccbe4ce19ddf6cb59c
Zeliwipin <zeliwipin@gmail.com>
2021-12-19 20:14:13 +0100

    implemented the user signup

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -2,27 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
 def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd['username'], password=cd['password'])
+    return render(request, 'usermanagement/login.html', {'form': LoginForm})
 
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
 
-                else:
-                    return HttpResponse('Disabled account')
-
-            else:
-                return HttpResponse('Invalid login')
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'usermanagement/signup.html', {'form': UserCreationForm})
     else:
-        form = LoginForm()
-    return render(request, 'usermanagement/login.html', {'form': form})
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')
