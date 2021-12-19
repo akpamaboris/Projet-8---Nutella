de8d8420b9735f1771f9b7652ee032bbb80d0977
Zeliwipin <zeliwipin@gmail.com>
2021-12-19 20:37:42 +0100

    implemented authentication logout and login

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('/login', views.user_login, name='login'),
-    path('/signup', views.signup, name='signup')
+    path('/signup', views.signup_user, name='signup'),
+    path('/logout', views.logout_user, name='logout')
 ]
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def user_login(request):
-    return render(request, 'usermanagement/login.html', {'form': LoginForm})
+    if request.method == 'GET':
+        return render(request, 'usermanagement/login.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form': AuthenticationForm(), 'error': 'username and password do not match'})
+        else:
+            login(request, user)
+            return redirect('home')
 
 
-def signup(request):
+def signup_user(request):
     if request.method == 'GET':
         return render(request, 'usermanagement/signup.html', {'form': UserCreationForm})
     else:
@@ -24,3 +33,10 @@ def signup(request):
             user.save()
             login(request, user)
             return redirect('home')
+        else:
+            return render(request, 'usermanagement/signup.html', {'form': UserCreationForm, 'error': 'Passwords do not match'})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
