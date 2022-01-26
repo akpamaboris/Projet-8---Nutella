ae9fda1f3432131691e2125037f0d4950cab37d0
Zeliwipin <zeliwipin@gmail.com>
2022-01-26 16:12:11 +0100

    working on deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,8 +14,8 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+# django_heroku.settings(locals())
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
