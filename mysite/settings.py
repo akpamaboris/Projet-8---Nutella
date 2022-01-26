e6d1bf999f723a86ba51172b162a27b55bbb5458
Zeliwipin <zeliwipin@gmail.com>
2022-01-26 17:59:05 +0100

    added more

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRETKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+]
 
 
 # Application definition
