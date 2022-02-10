e3f354e09f87f12c372f5031f058115fdfcf5ca2
Zeliwipin <zeliwipin@gmail.com>
2022-02-10 17:51:35 +0100

    added my first test

--- tests.py
+++ tests.py
@@ -1,11 +1,18 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Chrome
+import unittest
 
 
 class HomePageTest(LiveServerTestCase):
     def testdescription(self):
-        selenium = webdriver.Chrome()
-
+        selenium = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
         # Choose your url to visit
-        selenium.get('http://127.0.0.1:8000')
+        selenium.get('http://127.0.0.1:8000/')
+        assert 'Remy' in selenium.page_source
+        selenium.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
