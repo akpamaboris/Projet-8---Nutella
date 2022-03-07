d79b111d3bf5d64cd6d019d982fc7c69e146b0e0
Zeliwipin <zeliwipin@gmail.com>
2022-03-07 08:33:18 +0100

    one morecommit

--- tests.py
+++ tests.py
@@ -14,5 +14,29 @@ class HomePageTest(LiveServerTestCase):
         selenium.close()
 
 
+class ConnexionPageTest(LiveServerTestCase):
+    def testConnexion(self):
+        selenium = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+        selenium.get('http://127.0.0.1:8000/accountslogin')
+        assert 'Se connecter' in selenium.page_source
+        selenium.close()
+
+
+class RegisterPageTest(LiveServerTestCase):
+    def testConnexion(self):
+        selenium = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+        selenium.get('http://127.0.0.1:8000/accountssignup')
+        assert "S'enregistrer" in selenium.page_source
+        selenium.close()
+
+
+class ContactPageTest(LiveServerTestCase):
+    def testConnexion(self):
+        selenium = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+        selenium.get('http://127.0.0.1:8000/contactpage')
+        assert "Contactez" in selenium.page_source
+        selenium.close()
+
+
 if __name__ == "__main__":
     unittest.main()
